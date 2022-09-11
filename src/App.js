759d3d0d0c753ac3ea4b106534161f2d50330950
Nick Maslov <kategorychnost@gmail.com>
2022-09-11 01:28:32 -0400

    created project folder structure, layout, routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Blog from './pages/Blog';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
     return (
-        <div>
-            <button className='bg-red-500 p-2 rounded text-white'>
-                Tailwind Button
-            </button>
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/projects' element={<Projects />} />
+                    <Route path='/blog' element={<Blog />} />
+                    <Route path='/contact' element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ src/pages/Projects.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+function Projects() {
+    return <Layout>Projects</Layout>;
+}
+
+export default Projects;
